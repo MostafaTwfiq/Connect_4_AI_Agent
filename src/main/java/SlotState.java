b25b8bf940d9a472db17040b27b5f155bf91008c
Mohamed Medhat Saeed Hamada Zayton <es-mohamed.medhat2019@alexu.edu.eg>
2022-04-04 14:36:35 +0200

    setSlotValue

---
+++ src/main/java/SlotState.java
@@ -0,0 +1,5 @@
+public enum SlotState {
+    USER,
+    AGENT,
+    EMPTY,
+}
--- src/main/java/StateOperations.java
+++ src/main/java/StateOperations.java
@@ -2,17 +2,35 @@
 
 public class StateOperations {
 
-    public static byte[] setSlotValue(byte[] state, int num){
+    public static byte[] setSlotValue(byte[] state, int num, SlotState slotState){
+        int index = getSlotIndex(num);
+        int slot_num = num % 4;
+        byte value;
+        if (slotState == SlotState.AGENT)
+            value = 0b10;
+        else
+            value = 0b01;
+        var shift = slot_num == 0 ? 0 : (8-2*slot_num);
+        state[index] |= value << shift;
         return  state;
     }
 
-    public static byte[] getSlotValue(byte[] state, int num){
-        return  state;
+    public static SlotState getSlotValue(byte[] state, int num){
+        var index = getSlotIndex(num);
+        var b_arr = state[index];
+        return  SlotState.EMPTY;
     }
 
     public static int getEmptySlotCount(byte[] state, int num){
         return  1;
     }
 
+    private static int getSlotIndex(int num){
+        int index = (int) Math.floor(num / 4);
+        int slot_num = num % 4 ;
+        if (slot_num == 0 && num != 0)
+            index = (int) Math.floor((num-1) / 4);
+        return index;
+    }
 
 }
