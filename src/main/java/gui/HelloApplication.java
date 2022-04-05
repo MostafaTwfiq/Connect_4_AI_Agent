8e663a0b375dfb5da2ee33a12d179246ccd13c87
mazen sallam <mazensallam75@gmail.com>
2022-04-05 14:27:26 +0200

    initial commit

--- src/main/java/gui/HelloApplication.java
+++ src/main/java/gui/HelloApplication.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        System.out.println(HelloApplication.class.getName());
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
