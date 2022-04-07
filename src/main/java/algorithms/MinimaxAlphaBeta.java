2077443ae01155a0e180d32e876464c3d9c8414e
Mohamed Medhat Saeed Hamada Zayton <es-mohamed.medhat2019@alexu.edu.eg>
2022-04-07 22:13:21 +0200

    mimimax_alpha_beta

---
+++ src/main/java/algorithms/MinimaxAlphaBeta.java
@@ -0,0 +1,39 @@
+package algorithms;
+
+import javafx.util.Pair;
+import logic.StateOperations;
+
+public class MinimaxAlphaBeta {
+
+    public long decision(long state){
+        var value = maximize(state, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+        return value.getKey();
+    }
+
+    private Pair<long, double> maximize(long state, double alpha, double beta) {
+        if (StateOperations.isTerminal())
+            return StateOperations.evalu();
+        long maxChild = 0;
+        double maxUtility = Double.NEGATIVE_INFINITY;
+
+        for (c:StateOperations.getAgentSlots(state)) {
+            var value = minimize(state, alpha, beta);
+            var utility = value.getValue();
+            if (utility > maxUtility){
+                maxChild = value.getKey();
+                maxUtility = value.getValue();
+            }
+            if (maxUtility >= beta)
+                break;
+            if (maxUtility > alpha)
+                alpha = maxUtility
+        }
+
+        return new Pair<long, double>(maxChild, maxUtility);
+
+
+    }
+
+    private Pair<long, double> minimize(long state, double alpha, double beta) {
+    }
+}
